df6df641e90e2bf9a9f3455a70642d5fe79bdba5
Eren Tetik <96012085+erentetik@users.noreply.github.com>
2024-02-15 13:39:19 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
             <Route extact path="/" element={<SignIn />}  /> 
             <Route path="/ResetPassword" element={<ResetPassword />} />
             <Route path="/ActivateUser" element={<ActivateUser />} />
-            <Route path="setNewPassword/?=token${token}" element={<SetNewPassword/>} />
+            <Route path="setNewPassword/?=token:token" element={<SetNewPassword/>} />
           </Routes>
       </div>
     </Router>
